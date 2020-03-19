392311c7cb8d63fdbe4b8c0e9ead9f2b523beb1a
Barbara Klimek <baska.klimekova@gmail.com>
2020-03-19 10:57:02 +0100

    input & output exercises: print text with special characters with prinf function

---
+++ exercises/Input-output/VypisTextSoSpecialnymiZnakmi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){
+/* ak chcem napisat do konzoly znaky ako % alebo \ musime ich vo funkcii printf zdvojit %% a \\
+ ak chceme dat nieco do backslashu musime dat zapis \"slovo\" a vtedy ho uvedie vo funkcii printf v ""
+*/
+printf("James Bond \\ \"Agent 007\"  \\ # 150%% zaruka / \n"
+       "Spol. s rucenim obmedzenym"
+       );
+}
