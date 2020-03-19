f76cc3c3d4792a990bd0018ec4661f22da0753c5
Barbara Klimek <baska.klimekova@gmail.com>
2020-03-19 11:55:44 +0100

    update with comments

--- exercises/Input-output/ASCII/PrecitajAvypisZnakvASCII.c
+++ exercises/Input-output/ASCII/PrecitajAvypisZnakvASCII.c
@@ -9,7 +9,7 @@ printf("\n Zadaj jeden znak abecedy: ");
 c = getchar();
 
 cNew = c+1;
-
+//na zistenie ASCII kodu ti staci pre dany c znak dat nacitat jeho %d hodnotu
 printf("\nZadal si: %c (ASCII %d)",c,c);
 printf("\n%c (ASCII %d)",cNew,cNew);
 
