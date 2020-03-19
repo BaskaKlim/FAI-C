6e3a87949ae069dc5c5fe46529960641bed03e7e
Barbara Klimek <baska.klimekova@gmail.com>
2020-03-19 11:52:42 +0100

    Read one character from console and print nextone also with ASCII code: using getchar() function

---
+++ exercises/Input-output/ASCII/PrecitajAvypisZnakvASCII.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+//TODO: Program precita znak z konzoly a vypise nasledujuci znak aj jeho ASCII kod
+int c;
+int cNew;
+
+printf("\n Zadaj jeden znak abecedy: ");
+c = getchar();
+
+cNew = c+1;
+
+printf("\nZadal si: %c (ASCII %d)",c,c);
+printf("\n%c (ASCII %d)",cNew,cNew);
+
+return 0;
+
+}
+
