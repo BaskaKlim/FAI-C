84023559052a19ed8e16e3dfe478715cf65279d0
Barbara Klimek <baska.klimekova@gmail.com>
2020-03-20 23:32:45 +0100

    update %Xh

--- exercises/Input-output/HexadecimalValue/Hexadecimal.c
+++ exercises/Input-output/HexadecimalValue/Hexadecimal.c
@@ -5,5 +5,5 @@ int unsigned cislo;
 
 printf("Zadaj cislo od 0 do 255: ");
 scanf("%d", &cislo);
-printf("cislo %d je v hexadecimalnom tvare: %2X",cislo,cislo);
+printf("cislo %d je v hexadecimalnom tvare: %Xh",cislo,cislo);
 }
