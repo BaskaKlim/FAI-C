2ca4083de7c9a2392a9af702fb1197cbded5f6f7
Barbara Klimek <baska.klimekova@gmail.com>
2020-03-24 17:04:38 +0100

    using limits.h and Macros such as SCHAR_MAX,UCHAR_MAX, SHRT_MAX, etc

---
+++ exercises/Input-output/SignedAndUnsignedType/SignAndUnsignType.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <limits.h>
+
+
+
+int main(){
+/*
+HINT: -1 jako signed int je maximalni unsigned int a maximalni signed int je 1/2 maximalniho signed int
+*/
+//int a;
+//printf("zadaj cislo: ");
+//scanf("%d",&a);
+
+    printf("The maximum value of SIGNED CHAR = %d\n", SCHAR_MAX);
+    printf("The maximum value of UNSIGNED CHAR = %d\n", UCHAR_MAX);
+    printf("The maximum value of signed INT = %d\n", INT_MAX);
+    printf("The maximum value of Unsigned INT = %d\n", UINT_MAX);
+    printf("The maximum value of SHORT INT = %d\n", SHRT_MAX);
+    printf("The maximum value of LONG = %ld\n", LONG_MAX);
+
+return 0;
+
+
+
+}
