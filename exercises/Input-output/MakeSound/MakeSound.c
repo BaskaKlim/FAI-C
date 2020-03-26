7fa7fc96de75c45abf961777eb5795e661d91861
Barbara Klimek <baska.klimekova@gmail.com>
2020-03-26 14:11:47 +0100

    Make sound via using putchar('\a'); a= bell alert character

---
+++ exercises/Input-output/MakeSound/MakeSound.c
@@ -0,0 +1,20 @@
+/**********************************
+ *
+ * ucebnica_jazyka_C        ver. 1.0
+ * str45/ uloha 14
+ * ===============================
+ *
+ * barbara,     marec 2020
+ *
+ **********************************/
+
+/* systemove hlavickove subory */
+#include <stdio.h>
+
+/* funkcia main() */
+int main(void)
+{
+    // znak \a ma hodnotu 0x07 a vyznam: alert-BELL teda pisknutie, pripadne alert zvuk, ktory je nastaveny v operac.systeme
+  putchar('\a');  /* escape sekvencia */
+  return 0;
+}
