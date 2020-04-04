e139ed81b8bdaa8f6ce776aec5f58eee403fbfeb
Barbara Klimek <baska.klimekova@gmail.com>
2020-04-04 23:29:59 +0200

    adding if conditional to check if the file contains right number of inputs

--- exercises/Write&ReadFiles/ReadFromFile/ReadFromFile.c
+++ exercises/Write&ReadFiles/ReadFromFile/ReadFromFile.c
@@ -16,18 +16,19 @@ FILE *fr;
 int a,b,c;
 int sum;
 
-// optvorenie suboru (meno, typ)
+
 fr= fopen("data.txt","r");
-//nacitanie znakov cez fscanf podobnej ako scanf ale najprv ma deklarovany file, z ktoreho cita
-fscanf(fr, "%d,%d,%d,", &a,&b,&c);
-//check riadok, lebo mi posledne cislo nacitavalo v zlej hodnote, zistujem ake udaje sa nacitali do premennek
+//test ci subor obsahuje 3 vstupne hodnoty, fscanf()vracia pocet uspesne precitanych poloziek
+if (fscanf(fr, "%d,%d,%d,", &a,&b,&c)== 3) {
+
 printf("%d,%d,%d,\n",a,b,c); /**velmi dolezite je dat ciarky medzi %d alebo medzere a v takom formate mat aj file data **/
-//TODO: vypocitaj sucet a vypis ho
+
 sum =a+b+c;
 
 printf("%d\n",sum);
+} else
+printf("Soubor data.txt neobsahuje 3 realne cisla \n");
 
-//zatvorenie suboru a ukoncenie
 fclose(fr);
 return 0;
 
