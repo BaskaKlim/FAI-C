34236d1573ea651c9cf97204de4965804fce4600
Barbara Klimek <baska.klimekova@gmail.com>
2020-04-04 23:21:03 +0200

    read numbers from file, FIXED ERROR: last number didnt read corrrectly, using , between numbers and the same format in file

--- exercises/Write&ReadFiles/ReadFromFile/ReadFromFile.c
+++ exercises/Write&ReadFiles/ReadFromFile/ReadFromFile.c
@@ -11,17 +11,21 @@ ich soucet
 int main(){
 
 //definovanie premennej a suboru
-int a,b,c,sucet;
-FILE *fr;
-// optvorenie suboru (meno, typ)
-fr= fopen("demo.txt","r");
-//nacitanie znakov cez fscanf podobnej ako scanf ale najprv ma deklarovany file, z ktoreho cita
-fscanf(fr,"%d %d %d",&a,&b,&c);
-printf("%d,%d,%d ",a,b,c);
 
+FILE *fr;
+int a,b,c;
+int sum;
+
+// optvorenie suboru (meno, typ)
+fr= fopen("data.txt","r");
+//nacitanie znakov cez fscanf podobnej ako scanf ale najprv ma deklarovany file, z ktoreho cita
+fscanf(fr, "%d,%d,%d,", &a,&b,&c);
+//check riadok, lebo mi posledne cislo nacitavalo v zlej hodnote, zistujem ake udaje sa nacitali do premennek
+printf("%d,%d,%d,\n",a,b,c); /**velmi dolezite je dat ciarky medzi %d alebo medzere a v takom formate mat aj file data **/
 //TODO: vypocitaj sucet a vypis ho
-sucet = a+b+c;
-printf("%d\n",sucet);
+sum =a+b+c;
+
+printf("%d\n",sum);
 
 //zatvorenie suboru a ukoncenie
 fclose(fr);
